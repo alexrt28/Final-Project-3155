4fdf9710af000aefbb8709b3ab918993fbde14e1
Julian Dominguez <jdominguez9@gmail.com>
2026-04-22 16:57:09 -0400

    chore: clean numeric variable types

--- api/models/payment.py
+++ api/models/payment.py
@@ -9,7 +9,7 @@ class Payment(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     order_id = Column(Integer, ForeignKey("orders.id"))
     payment_type = Column(String(100), nullable=False)
-    card_info = Column(String, nullable=False)
+    card_info = Column(String(50), nullable=False)
     status = Column(String(100),nullable=False)
 
     order = relationship("Order", back_populates="payment")
